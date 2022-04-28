b1d06bef7aa3d7c3d7d3b601f4837a72011fd571
DanielaBeltranCruz <vi12982@innovaccion.mx>
2022-04-28 10:50:29 -0600

    Paso 6: Agregando nueva ruta que regresa un objeto

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.get('/launchx', (req, res) => {
     res.send('Welcome to LaunchX')
 })
 
+// Regresando un objeto
+// localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"}
+    res.send(explorer)
+})
+
 // Inicializando la app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
